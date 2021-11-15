5a39ef1030f27daa61e59b66260eb294ff674668
Monty Schmidt <monty@wuntusk.com>
2021-11-15 12:53:26 -0500

    chore:add lint

---
+++ apps/example-app/.eslintrc.js
@@ -0,0 +1,34 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    //'plugin:@typescript-eslint/recommended',
+    '@rushstack/eslint-config/profile/node',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@rushstack/typedef-var': ['off'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+      },
+    ],
+    'no-warning-comments': [
+      'warn',
+      { terms: ['todo', 'fixme'], location: 'anywhere' },
+    ],
+  },
+  ignorePatterns: ['**/dist/**/*.js', '**/dist/**/*.ts', '**/generated'],
+};
